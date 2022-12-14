95973867532614c737dcd8edce15b86e90fd9e51
Your Name <bakhtear2203@gmail.com>
2022-12-14 12:14:21 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// counter
+$('.counterr').counterUp({
+  delay: 5,
+  time: 2000
+});
+// nav part strt
+$('.nav_serch i').click(function(){
+  $('.nav_srchh').toggleClass('srch_bar')
+})
+// nav part endd
